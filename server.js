fbfbe0f61d7be5ae80fde8523bb4032da73eaf0f
Dev209clas3 <spammyspam431@gmail.com>
2024-06-20 12:42:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,77 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
+const cors = require('cors');
 const app = express();
+const port = 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 let books = [];
 
-function loadBooks() {
-    try {
-        const data = fs.readFileSync('books.json');
-        books = JSON.parse(data);
-    } catch (error) {
-        books = [];
-    }
-}
-
-function saveBooks() {
-    fs.writeFileSync('books.json', JSON.stringify(books, null, 2));
-}
-
-loadBooks();
-
-// GET all books
+// Endpoint to get the list of books
 app.get('/books', (req, res) => {
-    res.json(books);
+  res.json(books);
 });
 
-// GET a specific book by title
-app.get('/books/:title', (req, res) => {
-    const book = books.find(b => b.title === req.params.title);
-    if (book) {
-        res.json(book);
-    } else {
-        res.status(404).send('Book not found');
-    }
-});
-
-// POST a new book
+// Endpoint to add a new book
 app.post('/books', (req, res) => {
-    const newBook = req.body;
-    books.push(newBook);
-    saveBooks();
-    res.status(201).send('Book added');
+  const { title, author, progress } = req.body;
+  if (!title || !author || progress == null) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+  const newBook = { title, author, progress };
+  books.push(newBook);
+  res.status(201).json(newBook);
 });
 
-// PUT route to replace an entire book
-app.put('/books/:title', (req, res) => {
-    const title = req.params.title;
-    const newBook = req.body;
-    const bookIndex = books.findIndex(book => book.title === title);
-    if (bookIndex !== -1) {
-        books[bookIndex] = newBook;
-        saveBooks();
-        res.status(200).send('Book updated');
-    } else {
-        res.status(404).send('Book not found');
-    }
-});
-
-// POST route to update book progress
+// Endpoint to update book progress
 app.post('/books/updateProgress', (req, res) => {
-    const { title, progress } = req.body;
-    const bookIndex = books.findIndex(book => book.title === title);
-    if (bookIndex !== -1) {
-        books[bookIndex].progress = progress;
-        saveBooks();
-        res.status(200).send('Book progress updated');
-    } else {
-        res.status(404).send('Book not found');
-    }
+  const { title, progress } = req.body;
+  const book = books.find(b => b.title === title);
+  if (book) {
+    book.progress = progress;
+    res.json(book);
+  } else {
+    res.status(404).json({ error: 'Book not found' });
+  }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
