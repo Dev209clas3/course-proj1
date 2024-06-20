f81f71889e49b28656743713aa4cc17f43e1855b
Dev209clas3 <spammyspam431@gmail.com>
2024-06-20 12:28:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,77 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-
+const fs = require('fs');
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
 
-// Serve static files from the root directory
-app.use(express.static(path.join(__dirname)));
+let books = [];
 
-const dataFilePath = './data/books.json';
+function loadBooks() {
+    try {
+        const data = fs.readFileSync('books.json');
+        books = JSON.parse(data);
+    } catch (error) {
+        books = [];
+    }
+}
 
-const readData = () => {
-  const data = fs.readFileSync(dataFilePath);
-  return JSON.parse(data);
-};
+function saveBooks() {
+    fs.writeFileSync('books.json', JSON.stringify(books, null, 2));
+}
 
-const writeData = (data) => {
-  fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
-};
+loadBooks();
 
+// GET all books
 app.get('/books', (req, res) => {
-  const data = readData();
-  res.json(data);
+    res.json(books);
 });
 
+// GET a specific book by title
+app.get('/books/:title', (req, res) => {
+    const book = books.find(b => b.title === req.params.title);
+    if (book) {
+        res.json(book);
+    } else {
+        res.status(404).send('Book not found');
+    }
+});
+
+// POST a new book
 app.post('/books', (req, res) => {
-  const newBook = req.body;
-  const data = readData();
-  data.push(newBook);
-  writeData(data);
-  res.status(201).json(newBook);
+    const newBook = req.body;
+    books.push(newBook);
+    saveBooks();
+    res.status(201).send('Book added');
 });
 
-// Serve the main HTML file
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+// PUT route to replace an entire book
+app.put('/books/:title', (req, res) => {
+    const title = req.params.title;
+    const newBook = req.body;
+    const bookIndex = books.findIndex(book => book.title === title);
+    if (bookIndex !== -1) {
+        books[bookIndex] = newBook;
+        saveBooks();
+        res.status(200).send('Book updated');
+    } else {
+        res.status(404).send('Book not found');
+    }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// POST route to update book progress
+app.post('/books/updateProgress', (req, res) => {
+    const { title, progress } = req.body;
+    const bookIndex = books.findIndex(book => book.title === title);
+    if (bookIndex !== -1) {
+        books[bookIndex].progress = progress;
+        saveBooks();
+        res.status(200).send('Book progress updated');
+    } else {
+        res.status(404).send('Book not found');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
